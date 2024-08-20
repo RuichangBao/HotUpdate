ee38ca2fafe5a895af057f36e8a56c987302ad91
RuichangBao <1294838346@qq.com>
2024-08-20 20:38:28 +0800

    预下载

--- UnityHotUpdate/Assets/Scripts/Init.cs
+++ UnityHotUpdate/Assets/Scripts/Init.cs
@@ -1,49 +1,125 @@
-using UnityEngine;
-using System.IO;
 using System.Collections;
-using UnityEngine.Networking;
-using System.Text;
-using UnityEngine.SceneManagement;
-using UnityEditor;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.AddressableAssets.ResourceLocators;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.UI;
 
+// 检测更新并下载资源
 public class Init : MonoBehaviour
 {
-    private string url = @"http://192.168.3.88:8080";
-    private string listPath = "list.txt"; 
-    private static string platform = "StandaloneWindows64";
-    private string savePath = $"{Application.streamingAssetsPath}/{platform}/";
-   
+    /// <summary>
+    /// 显示下载状态和进度
+    /// </summary>
+    public Text updateText;
+
+    /// <summary>
+    /// 重试按钮
+    /// </summary>
+    public Button retryBtn;
+
     void Start()
     {
-        StartCoroutine(GetFileList());
+        retryBtn.gameObject.SetActive(false);
+        retryBtn.onClick.AddListener(() =>
+        {
+            StartCoroutine(DoUpdateAddressadble());
+        });
+
+        // 默认自动执行一次更新检测
+        StartCoroutine(DoUpdateAddressadble());
     }
 
-    ///<summary>获取文件列表</summary>
-    private IEnumerator GetFileList()
+    IEnumerator DoUpdateAddressadble()
     {
-        UnityWebRequest unityWebRequest = UnityWebRequest.Get($"{url}/{listPath}");
-        yield return unityWebRequest.SendWebRequest();
+        AsyncOperationHandle<IResourceLocator> initHandle = Addressables.InitializeAsync();
+        yield return initHandle;
 
-        string allFiles = Encoding.UTF8.GetString(unityWebRequest.downloadHandler.data);
-        //unityWebRequest.Dispose();
-        string[] downLoadFiles = allFiles.Split("\r\n");
-        for (int i = 0; i < downLoadFiles.Length; i++)
+        // 检测更新
+        var checkHandle = Addressables.CheckForCatalogUpdates(true);
+        yield return checkHandle;
+        if (checkHandle.Status != AsyncOperationStatus.Succeeded)
         {
-            if (!string.IsNullOrEmpty(downLoadFiles[i]))
+            OnError("CheckForCatalogUpdates Error\n" + checkHandle.OperationException.ToString());
+            yield break;
+        }
+
+        if (checkHandle.Result.Count > 0)
+        {
+            var updateHandle = Addressables.UpdateCatalogs(checkHandle.Result, true);
+            yield return updateHandle;
+
+            if (updateHandle.Status != AsyncOperationStatus.Succeeded)
             {
-                DownloadFile(downLoadFiles[i]);
+                OnError("UpdateCatalogs Error\n" + updateHandle.OperationException.ToString());
+                yield break;
+            }
+
+            // 更新列表迭代器
+            List<IResourceLocator> locators = updateHandle.Result;
+            foreach (var locator in locators)
+            {
+                List<object> keys = new List<object>();
+                keys.AddRange(locator.Keys);
+                // 获取待下载的文件总大小
+                var sizeHandle = Addressables.GetDownloadSizeAsync(keys.GetEnumerator());
+                yield return sizeHandle;
+                if (sizeHandle.Status != AsyncOperationStatus.Succeeded)
+                {
+                    OnError("GetDownloadSizeAsync Error\n" + sizeHandle.OperationException.ToString());
+                    yield break;
+                }
+
+                long totalDownloadSize = sizeHandle.Result;
+                updateText.text = updateText.text + "\ndownload size : " + totalDownloadSize;
+                Debug.Log("download size : " + totalDownloadSize);
+                if (totalDownloadSize > 0)
+                {
+                    // 下载
+                    var downloadHandle = Addressables.DownloadDependenciesAsync(keys, true);
+                    while (!downloadHandle.IsDone)
+                    {
+                        if (downloadHandle.Status == AsyncOperationStatus.Failed)
+                        {
+                            OnError("DownloadDependenciesAsync Error\n" + downloadHandle.OperationException.ToString());
+                            yield break;
+                        }
+                        // 下载进度
+                        float percentage = downloadHandle.PercentComplete;
+                        Debug.Log($"已下载: {percentage}");
+                        updateText.text = updateText.text + $"\n已下载: {percentage}";
+                        yield return null;
+                    }
+                    if (downloadHandle.Status == AsyncOperationStatus.Succeeded)
+                    {
+                        Debug.Log("下载完毕!");
+                        updateText.text = updateText.text + "\n下载完毕";
+                    }
+                }
             }
         }
-        SceneManager.LoadScene("SampleScene");
+        else
+        {
+            updateText.text = updateText.text + "\n没有检测到更新";
+        }
+
+        // 进入游戏
+        EnterGame();
     }
-    void DownloadFile(string fileName)
+
+    // 异常提示
+    private void OnError(string msg)
     {
-        UnityWebRequest unityWebRequest = UnityWebRequest.Get($"{url}/{platform}/{fileName}");
-        unityWebRequest.downloadHandler = new DownloadHandlerFile($"{savePath}/{fileName}");
-        FileInfo file = new FileInfo(savePath);
-        unityWebRequest.SendWebRequest();
-        while (!unityWebRequest.isDone)
-        { }
-        unityWebRequest.Dispose();
+        updateText.text = updateText.text + $"\n{msg}\n请重试! ";
+        // 显示重试按钮
+        retryBtn.gameObject.SetActive(true);
+    }
+
+
+    // 进入游戏
+    void EnterGame()
+    {
+        // TODO
     }
 }
