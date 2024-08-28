25e457821e8fef2ac28a276535ab354711765bb1
RuichangBao <1294838346@qq.com>
2024-08-28 17:18:20 +0800

    PC包

---
+++ 001/UnityHotUpdate1_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Main_CodeGen.c
@@ -0,0 +1,77 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern void Init_Start_m1959B08792E3EF091173419AA86851BB8C82D080 (void);
+extern void Init_InitAddressable_m4D3F9DF27D23B56C4C0638B25B1685C81390ECE9 (void);
+extern void Init_InitializeAsyncCompleted_m8524ED076F24A567BD988795C78AADAFA5370546 (void);
+extern void Init_CheckForCatalogUpdatesCompleted_m986A7848EEA0E635942C8D4B92AE9A81C659883B (void);
+extern void Init_BtnOnClick_m2D9393F845D9B6BCF0BD5DBC55A1EB7BB96ED59E (void);
+extern void Init__ctor_mBB73AC234E7E199F46AF2A204028921051220722 (void);
+extern void Main_Start_mF75DAF4781CA471440DE54F931CEF5E5C53F780C (void);
+extern void Main_OnLoaded_m8F2695C1D7A5A5AEDF6646C4921F4D5D5A665C19 (void);
+extern void Main_OnDestroy_mDA8C1FBD7FA5C6625DEA3C202632E5F5C8AE9CA2 (void);
+extern void Main__ctor_m12E7E178E99C46C21D417CC411404F66037D9062 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mEA134AEC1ED942E87BF9065F6614CD223FCE7EB2 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_m386F87FC7A566D3AB658F149EC658DC8F94FFBDE (void);
+static Il2CppMethodPointer s_methodPointers[12] = 
+{
+	Init_Start_m1959B08792E3EF091173419AA86851BB8C82D080,
+	Init_InitAddressable_m4D3F9DF27D23B56C4C0638B25B1685C81390ECE9,
+	Init_InitializeAsyncCompleted_m8524ED076F24A567BD988795C78AADAFA5370546,
+	Init_CheckForCatalogUpdatesCompleted_m986A7848EEA0E635942C8D4B92AE9A81C659883B,
+	Init_BtnOnClick_m2D9393F845D9B6BCF0BD5DBC55A1EB7BB96ED59E,
+	Init__ctor_mBB73AC234E7E199F46AF2A204028921051220722,
+	Main_Start_mF75DAF4781CA471440DE54F931CEF5E5C53F780C,
+	Main_OnLoaded_m8F2695C1D7A5A5AEDF6646C4921F4D5D5A665C19,
+	Main_OnDestroy_mDA8C1FBD7FA5C6625DEA3C202632E5F5C8AE9CA2,
+	Main__ctor_m12E7E178E99C46C21D417CC411404F66037D9062,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mEA134AEC1ED942E87BF9065F6614CD223FCE7EB2,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_m386F87FC7A566D3AB658F149EC658DC8F94FFBDE,
+};
+static const int32_t s_InvokerIndices[12] = 
+{
+	4844,
+	4844,
+	3628,
+	3624,
+	4844,
+	4844,
+	4844,
+	3627,
+	4844,
+	4844,
+	7113,
+	4844,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_Main_CodeGenModule;
+const Il2CppCodeGenModule g_Main_CodeGenModule = 
+{
+	"Main.dll",
+	12,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+};
