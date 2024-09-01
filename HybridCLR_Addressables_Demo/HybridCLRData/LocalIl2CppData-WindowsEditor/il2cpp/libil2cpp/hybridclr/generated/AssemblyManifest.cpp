6abf9603fd7f857a384a42a2c42afe69accfd1d2
RuichangBao <1294838346@qq.com>
2024-09-01 20:51:09 +0800

    dsaf

---
+++ HybridCLR_Addressables_Demo/HybridCLRData/LocalIl2CppData-WindowsEditor/il2cpp/libil2cpp/hybridclr/generated/AssemblyManifest.cpp
@@ -0,0 +1,13 @@
+#include "../Il2CppCompatibleDef.h"
+
+namespace hybridclr
+{
+	const char* g_placeHolderAssemblies[] =
+	{
+		//!!!{{PLACE_HOLDER
+		"GamePlay",
+
+		//!!!}}PLACE_HOLDER
+		nullptr,
+	};
+}
